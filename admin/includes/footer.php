cd03f03c64d87b725a47c811f0350971f67d010f
Tousif Mahmud Emon <tousifemon954@gmail.com>
2024-12-05 12:03:24 +0600

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; BDU Library Management System |<a href="https://bdu.ac.bd/" target="_blank"  > Designed by : Tousif & Opu</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
